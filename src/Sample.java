3fe06b8d55cbde58eeb1eba51f73d5c7c929ca43
ajit <ajitwakade@gmail.com>
2018-10-13 11:44:20 +0530

    this is first check in

---
+++ src/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+
+	public Sample() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
